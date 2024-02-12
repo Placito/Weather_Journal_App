d268f0023dd3d6c3f8513178b8e00d5fc08fb379
Mariana <101410421+Placito@users.noreply.github.com>
2024-02-12 17:48:09 +0000

    set the structure

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+//Javascript object
+projectData = {};
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const bodyParser = require('body-parser')
  
 app.use(cors())
- 
+
 app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'})
 })
@@ -12,5 +16,3 @@ app.listen(3000, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
 
-//Javascript object
-projectData = {}
--- website/app.js
+++ website/app.js
@@ -1,2 +1,37 @@
 // Personal API Key for OpenWeatherMap API
 const apiKey = '<your_api_key>&units=imperial';
+
+// Select the element with id 'generate'
+const generateButton = document.getElementById('generate');
+
+// Define the named callback function
+function handleClick(event) {
+    event.preventDefault()
+
+    console.log('Button clicked!');
+}
+
+// Add an event listener to the 'generate' button
+generateButton.addEventListener('click', handleClick);
+
+// Function to GET Project Data
+const retrieveData = async () =>{
+    const request = await fetch('/all');
+    try {
+    // Transform into JSON
+    const allData = await request.json()
+    console.log(allData)
+    // Write updated data to DOM elements
+    document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
+    document.getElementById('content').innerHTML = allData.feel;
+    document.getElementById("date").innerHTML =allData.date;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+   }
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
